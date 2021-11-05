93ec6db2efe6e84cd5bbd7f9e5fa614386238655
Marujinho <aka.contato@gmail.com>
2021-11-04 23:06:56 -0300

    adding links with parameters to a global array

--- sandslash/sandslash/spiders/scratch.py
+++ sandslash/sandslash/spiders/scratch.py
@@ -1,10 +1,14 @@
 import scrapy
+import pandas as pd
 
 #enter url
 class Scratch(scrapy.Spider):
     name = 'scratch'
+    base = 'https://br.indeed.com/'
     urls = []
+    regularLinks = []
     endpoints = []
+    linksWithParameters = []
 
     def start_requests(self):
         urls = [
@@ -18,19 +22,31 @@ class Scratch(scrapy.Spider):
         links = response.css('a::attr(href)').getall()
 
         for link in links:
-            self.endpoints.append(link)
+            self.filterUrl(link)
 
-        print(self.endpoints)
+        self.printAll()
 
-        # for quote in response.xpath('//a[contains(@href, "*")]'):
-        #     page = quote.css('a::attr(href)').getall()
-        #
-        #     if page[0] not in self.urls:
-        #
-        #         self.urls.append(page[0])
-        #
-        #         yield {
-        #             'endpoint': page[0]
-        #         }
+    def filterUrl(self, url):
+        link = url.split('.com/')
+
+        if(len(link) > 1):
+            #self.endpoints.append(link[1])
+
+            str_1 = pd.Series([link[1]])
+            has_parameter = str_1.str.contains('?', regex=False)
+
+            # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            # print(has_parameter[0])
+
+            if has_parameter[0]:
+                self.linksWithParameters.append(link[1])
+
+            #yield {'regularLink':link[0] }
+            #print(self.endpoints)
+            #yield {'withParameters':link[1] }
+
+    def printAll(self):
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        print(self.linksWithParameters)
 
 #get all links
