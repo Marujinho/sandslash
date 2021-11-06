fbe12febeb9c92111a5d6d8279bf3339a9a1e89e
Marujinho <aka.contato@gmail.com>
2021-11-06 17:25:10 -0300

    saving urls and urls with parameters

--- sandslash/sandslash/spiders/scratch.py
+++ sandslash/sandslash/spiders/scratch.py
@@ -1,5 +1,6 @@
 import scrapy
 import pandas as pd
+import requests
 
 #enter url
 class Scratch(scrapy.Spider):
@@ -9,14 +10,19 @@ class Scratch(scrapy.Spider):
     regularLinks = []
     endpoints = []
     linksWithParameters = []
+    visitedEndpoints = []
+    counter = 0
+
+    def __init__(self):
+        self.endpoints.append(self.base)
 
     def start_requests(self):
-        urls = [
-            'https://br.indeed.com/'
-        ]
+        #urls = self.endpoints
 
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        for url in self.endpoints:
+            if url not in self.visitedEndpoints:
+                yield scrapy.Request(url=url, callback=self.parse)
+                self.visitedEndpoints.append(url)
 
     def parse(self, response, **kwargs):
         links = response.css('a::attr(href)').getall()
@@ -27,19 +33,22 @@ class Scratch(scrapy.Spider):
         self.printAll()
 
     def filterUrl(self, url):
+        print("Doing my thing...")
         link = url.split('.com/')
 
         if(len(link) > 1):
-            #self.endpoints.append(link[1])
-
+            r = requests.get(self.base + link[1])
             str_1 = pd.Series([link[1]])
             has_parameter = str_1.str.contains('?', regex=False)
 
-            # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-            # print(has_parameter[0])
+            if (r.status_code == 200):
+                self.endpoints.append(self.base + link[1])
 
-            if has_parameter[0]:
-                self.linksWithParameters.append(link[1])
+                if (has_parameter[0]):
+                    self.linksWithParameters.append(self.base + link[1])
+
+
+        return self.linksWithParameters
 
             #yield {'regularLink':link[0] }
             #print(self.endpoints)
@@ -48,5 +57,6 @@ class Scratch(scrapy.Spider):
     def printAll(self):
         print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
         print(self.linksWithParameters)
+        return self.start_requests()
 
 #get all links
